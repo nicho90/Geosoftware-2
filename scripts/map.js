db1b67282be8188db67b1cab3351252f35337ec0
ReneUnrau <renegolas@web.de>
2014-01-07 17:49:43 +0100

    Added GoogleSatellite Layer
    -Layer is now available

--- scripts/map.js
+++ scripts/map.js
@@ -11,11 +11,26 @@ window.onload = function() {
 
 function drawMap(){
 	// create a map in the "map" div, set the view to a given place and zoom
-	var map = L.map('map').setView([51.96, 7.62], 18);
+	
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+    var osm = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	});
+	var google = new L.Google('SATELLITE');
+	  
+	
+	var layer = {
+		"OSM": osm,
+		"Google": google
+	};
+	  
+	var map = L.map('map', {
+		center: new L.LatLng(51.96, 7.62),
+		zoom: 18,
+		layers: [osm]
+	});
+	
+	L.control.layers(layer).addTo(map);
     
 	mainMap = map;
 	mainMap.on('moveend', drawMeasurements);
@@ -30,24 +45,23 @@ function drawMeasurements(){
 	var swLng = bounds.getSouthWest().lng;
 	
 	for(var i=0; i < markers.length; i++){
-		mainMap.removeLayer(markers[i]);
-	}
+        mainMap.removeLayer(markers[i]);
+    }
 	
 	$.getJSON("https://envirocar.org/api/stable/rest/measurements?bbox=" + swLng + "," + swLat + "," + neLng + "," + neLat,function(result){
 	
-		result = result.features;
+		result = result.features;		
 		
 		$.each(result, function(i, result){
 		
-			marker = L.marker([result.geometry.coordinates[1], result.geometry.coordinates[0]], {icon: redDot});
-				
+			marker = L.marker([result.geometry.coordinates[1], result.geometry.coordinates[0]], {icon: redDot})
+				 .bindPopup('A pretty CSS3 popup. <br> Easily customizable.');
+				 
 			markers.push(marker);
 		});
-	
-		for(var i=0; i < markers.length; i++){
+		
+		for(var i = 0; i < markers.length; i++){
 			mainMap.addLayer(markers[i]);
 		}
 	});
-	
-	
 }
