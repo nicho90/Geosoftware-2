7dac43f3bb21448be15cdaa6d8972645c1d4ce1f
JoMol <jo_hanna92@web.de>
2014-02-11 14:25:01 +0100

    Diagramm Popup
    - Achseneinteilung angepasst
    - Farbe der Balken geändert
    
    comitted with Nicho

--- scripts/diagram.js
+++ scripts/diagram.js
@@ -4,12 +4,13 @@ function diagrams() {
 $('#diagram').html('<div id=diagramBar class=popuplinks>'+
 						'<ul>'+
 							'<li><a href="">Geschwindigkeit</a></li>'+
+							'<li><a href="">CO2-Ausstoss</a></li>'+
 							'<li><a href="">Spritverbrauch</a></li>'+
 							'<li><a href="">MAF</a></li>'+
 							'<li><a href="">Fahrzeugtypen</a></li>'+
 						'</ul>'+
 					'</div>'+
-					'<div id=container style="width: 80%; height: 80%; margin: 0 auto;"></div>');
+					'<div id=container style="width: 100%; height: 90%; margin: 0 auto;"></div>');
 		$('#diagram').dialog({ 
 		autoOpen: true,   
 		modal: true,
@@ -22,21 +23,59 @@ $(function () {
                 type: 'column'
             },
             title: {
-                text: 'Monthly Average Rainfall'
+                text: 'Verteilung der Geschwindigkeit'
             },
             subtitle: {
-                text: 'Source: WorldClimate.com'
+                text: ''
             },
             xAxis: {
                 categories: [
-                    'Autotypen'
+                    '0-5',
+					'5-10',
+					'10-15',
+					'15-20',
+					'20-25',
+					'25-30',
+					'30-35',
+					'35-40',
+					'40-45',
+					'45-50',
+					'50-55',
+					'55-60',
+					'60-65',
+					'65-70',
+					'70-75',
+					'75-80'
+					/*
+					'80-85',
+					'85-90',
+					'90-95',
+					'95-100',
+					'100-105',
+					'105-110',
+					'110-115',
+					'115-120',
+					'120-125',
+					'125-130',
+					'130-135',
+					'135-140',
+					'140-145',
+					'145-150',
+					'150-155',
+					'155-160',
+					'160-165',
+					'165-170',
+					'170-175',
+					'175-180',
+					'>180'
+					*/
                 ]
             },
             yAxis: {
                 min: 0,
 				max: 100,
                 title: {
-                    text: 'Häufigkeit [%]'
+                    text: 'Haeufigkeit [%]'
                 }
             },
             tooltip: {
@@ -54,20 +93,25 @@ $(function () {
                 }
             },
             series: [{
-                name: 'Autotyp 4',
-                data: [20]
-    
-            }, {
-                name: 'Autotyp 3',
-                data: [20]
-    
-            }, {
-                name: 'Autotyp 2',
-                data: [30]
-    
-            }, {
-                name: 'Autotyp 1',
-                data: [30]
+                name: 'Geschwindigkeit [km/h]',
+                data: [ 0, 
+						20, 
+						21, 
+						50,
+						1, 
+						6, 
+						5, 
+						80, 
+						40, 
+						20, 
+						90, 
+						100, 
+						20, 
+						80,
+						3,
+						6
+					],
+				color: '#8CBC3E'
     
             }]
         });
