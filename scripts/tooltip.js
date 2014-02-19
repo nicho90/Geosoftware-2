d6f2b286d7d5ee2b406d737ca8b6aba9c1cdc11e
Nicho <n.schiestel@uni-muenster.de>
2014-02-19 18:04:54 +0100

    Anzahl der Messpunkte
    Anzahl der Messpunkte jetzt in eigener Box, oben im Headbereich
    
    (committed mit Johanna)

--- scripts/tooltip.js
+++ scripts/tooltip.js
@@ -4,5 +4,6 @@
                 $('#chooseSinglePoint').tipsy({fade:false, gravity: 'n'});
                 $('#chooseTrack').tipsy({fade:false, gravity: 'n'});
                 $('#choosePolygon').tipsy({fade:false, gravity: 'n'}); 
-                $('#interpolationInfo').tipsy({fade:false, gravity: 'e'}); 
+                $('#interpolationInfo').tipsy({fade:false, gravity: 'e'});
+                $('#maxOfMeasurementsInfo').tipsy({fade:false, gravity: 'n'});
 });
