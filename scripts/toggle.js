2f935165a4f891b0f4b4e98032ae1154f8cca4c2
Nicho <n.schiestel@uni-muenster.de>
2014-02-20 15:50:25 +0100

    Design- und Layoutupdate
    - endlich wieder da, der Footer
    - Downloadbutton für alle Messwerte hinzugefügt (in der Sidebar bei der
    Vorschau der selektierten Punkte)
    - Interpolationsbox geändert (Radiobuttons, statt Dropdownauswahl bei
    der Interpolationsmethode)
    - Vorbereitung für Kriging-Parametrisierung (wenn der Radio-Button bei
    Kriging angeklickt wird, erscheinen die Inputfelder für die
    Parametrisierung)
    
    Hinweis: Die Interpolationen funtkionieren zur Zeit nicht, da sich das
    Design geändert hat und Teile verlagert werden
    
    (committed mit Johanna)

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -7,3 +7,11 @@ function toggle_visibility(id) {
                         e.style.display = 'none';
                 }
 }
+
+
+// function to show Kriging-options if Radiobutton is selected 
+function showhidediv(rad){
+    var rads=document.getElementsByName(rad.name);
+    document.getElementById('idwBox').style.display=(rads[0].checked)?'block':'none' ;
+    document.getElementById('krigingBox').style.display=(rads[1].checked)?'block':'none' ;
+}
