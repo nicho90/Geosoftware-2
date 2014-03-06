9512b0e8747200f12a7bc1af646a4b34c1f5f589
JoMol <jo_hanna92@web.de>
2014-03-06 16:40:21 +0100

    .js-Datei für Download der selektierten Punkte

---
+++ scripts/download_messpunkte.js
@@ -0,0 +1,40 @@
+function download_sel_pkte(){
+
+if(selection.length > 0){
+
+	var jsonString = '{"measurements": [';
+
+	$.each(selection, function(i, measurement){
+			lat = selection[i].geometry.coordinates[1];
+			lon = selection[i].geometry.coordinates[0];
+			point = i + 1;
+			jsonString = jsonString + '{"latitude":'+selection[i].geometry.coordinates[1]+ ' , ' +  '"longitude":' + selection[i].geometry.coordinates[0]+ ' , ' + '"sensorID":' + '"'+ measurement.properties.sensor.properties.id + '"'+ ' , ' + '"time":' + '"'+ measurement.properties.time + '"'+  ' , ' + 	'"manufacturer":' + '"'+ measurement.properties.sensor.properties.manufacturer + '"'+ ' , ' + '"model":' + '"'+ measurement.properties.sensor.properties.model + '"'+ ' , ' + 	'"speed":'+ measurement.properties.phenomenons.Speed.value + ' , ' + '"CO2":' +  measurement.properties.phenomenons.CO2.value + ' , ' + '"consumption":' + measurement.properties.phenomenons.Consumption.value + ' , ' + '"MAF":' + measurement.properties.phenomenons.MAF.value  + "}" + ",";	
+		});
+	var jsonStringF = jsonString.substr(0, jsonString.length-1);
+	var jsonResult = jsonStringF + '] }';
+
+	var jsonResultFile = "text/json;charset=utf-8," + escape(JSON.stringify(eval("(" + jsonResult + ")")));
+
+	$('#downloadMpDialog').html('Die ausgew&auml;hlten Messpunkte werden als .json-Datei heruntergeladen.<br>');
+					
+		$('#downloadMpDialog').dialog({ 
+			height: 150,
+			width: 600,
+			autoOpen: true,   
+			modal: true,
+		});	
+
+	var name = new Date();
+	$('<a href="data:' + jsonResultFile + '" download="' + name.getTime() + '.json"><div style="text-align: right;"><input type="button" name="confirmDownload" value="Okay"></div></a>').appendTo('#downloadMpDialog');
+	//$('#downloadMpDialog').dialog('close');	
+	}
+else{
+	var downloadmpDialog = $('<p>Um die Downloadfunktion nutzen zu k&ouml;nnen, muss sich mindestens ein Punkt in der Wertetabelle befinden.<br>Bitte selektieren Sie einen oder mehrere Punkte.</p>').dialog({
+			buttons: {
+				"Okay": function() {downloadmpDialog.dialog('close');}
+			},
+			width: 600
+		});
+	}
+
+}
