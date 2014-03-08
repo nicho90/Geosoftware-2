8a7069ca4ab99fd4b90ba4c4248ba70c51837128
Nicho <n.schiestel@uni-muenster.de>
2014-03-08 02:05:06 +0100

    Prüfung der Nutzereingaben
    - Bei Kriging sind die Werte nun vorgeben, sodass man sie nicht erst
    noch eintragen muss
    - Es findet eine Überprüfung statt, ob der Nutzer die Parameter richtig
    ausgefüllt hat (Werte zwischen 1 und 100)

---
+++ scripts/checkinput.js
@@ -0,0 +1,41 @@
+function checkUserInput() {
+
+var firstParameter = false;    
+var secondParameter = false;  
+    
+    if(document.getElementById('alpha').value <= 100 && document.getElementById('alpha').value > 0) {
+        firstParameter = true;
+    }
+    
+    else {  
+            firstParameter = false;
+            var dialog = $('<p>Bitte tragen Sie für Alpha eine Zahl zwischen 1 und 100 ein.</p>').dialog({
+                width:600,
+                height:200,
+                modal: true,
+                buttons: {
+                    "OK":  function() {dialog.dialog('close');}
+ 			    }
+ 		     });
+    }
+    
+    if(document.getElementById('sigmatwo').value <= 100 && document.getElementById('sigmatwo').value > 0) {
+        secondParameter = true;
+    }
+    
+    else {  
+            secondParameter = false;
+            var dialog = $('<p>Bitte tragen Sie für Simga<sup>2</sup> eine Zahl zwischen 1 und 100 ein.</p>').dialog({
+                modal: true,
+                width:600,
+                height:200,
+                buttons: {
+                    "OK":  function() {dialog.dialog('close');}
+ 			    }
+ 		     });
+    }
+    
+    if(firstParameter && secondParameter) {
+        startInterpolation();
+    }
+}
